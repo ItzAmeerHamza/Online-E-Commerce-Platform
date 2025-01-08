5cc603daebec0c8b7171b32265341fe7f96d4113
ItzAmeerHamza <ameer.hamza.nu@gmail.com>
2025-01-08 23:22:45 +0500

    fixes

--- client/typings.d.ts
+++ client/typings.d.ts
@@ -1,82 +1,85 @@
 interface Product {
-    id: string;
-    slug: string;
-    title: string;
-    price: number;
-    rating: number;
-    description: string;
-    mainImage: string;
-    manufacturer: string;
-    categoryId: string;
-    category: {name: string}?;
-    inStock: number;
-  }
+  id: string;
+  slug: string;
+  title: string;
+  price: number;
+  rating: number;
+  description: string;
+  mainImage: string;
+  manufacturer: string;
+  categoryId: string;
+  category: {name: string}?;
+  inStock: number;
+}
   
-  interface SingleProductPageProps {
-    params: {
-      productSlug: string;
-    };
-  }
-  
-  type ProductInWishlist = {
-    id: string;
-    title: string;
-    price: number;
-    image: string;
-    slug: string;
-    stockAvailabillity: number;
+interface SingleProductPageProps {
+  params: {
+    productSlug: string;
   };
+}
   
-  interface OtherImages {
-    imageID: number;
-    productID: number;
-    image: string;
-  }
+type ProductInWishlist = {
+  id: string;
+  title: string;
+  price: number;
+  image: string;
+  slug: string;
+  stockAvailabillity: number;
+};
   
-  interface Category {
-    id: string;
-    name: string;
-  }
+interface OtherImages {
+  imageID: number;
+  productID: number;
+  image: string;
+}
   
-  interface User {
-    id: string;
-    email: string;
-    password: string | null;
-    role: string;
-  }
+interface Category {
+  id: string;
+  name: string;
+}
   
-  interface Order {
-    id: string;
-    adress: string;
-    apartment: string;
-    company: string;
-    dateTime: string;
-    email: string;
-    lastname: string;
-    name: string;
-    phone: string;
-    postalCode: string;
-    status: "processing" | "canceled" | "delivered";
-    city: string;
-    country: string;
-    orderNotice: string?;
-    total: number;
-  }
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  age: age;
+  email: string;
+  password: string | null;
+}
   
-  interface SingleProductBtnProps {
-    product: Product;
-    quantityCount: number;
-  }
-  
-  
-  interface Category {
-    id: string;
-    name: string;
-  }
-  
-  interface WishListItem {
-    id: string;
-    userId: string;
-    productId: string;
-    product: Product;
-  }
+interface Order {
+  id: string;
+  adress: string;
+  apartment: string;
+  company: string;
+  dateTime: string;
+  email: string;
+  lastname: string;
+  name: string;
+  phone: string;
+  postalCode: string;
+  status: "processing" | "canceled" | "delivered";
+  city: string;
+  country: string;
+  orderNotice: string?;
+  total: number;
+}
+
+interface SingleProductBtnProps {
+  product: Product;
+  quantityCount: number;
+}
+
+
+interface Category {
+  id: string;
+  name: string;
+}
+
+interface WishListItem {
+  id: string;
+  userId: string;
+  productId: string;
+  product: Product;
+}
